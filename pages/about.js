64910c79333b55494aaef096b6b9feeac4dc381a
Granev Kirill <k.e.granev@gmail.com>
2019-11-16 15:59:02 +0300

    Functional component

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+
+// functional component
+const About = () => {
+    const message = 'Hello world';
+    return (
+        <h1>Hello About Page - {message}</h1>        
+    )
+};
+
+export default About;
