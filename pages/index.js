56fa10e2cabbe67a63c04950fd8f6b9065d1b4d3
Granev Kirill <k.e.granev@gmail.com>
2019-11-16 18:57:28 +0300

    Some basic styles

--- pages/index.js
+++ pages/index.js
@@ -17,28 +17,35 @@ const Home = () => (
     </Head>
 
     <Navbar />
+    
+    <div className="home-page">
+      <div className="container">
+        <div className="row">
 
-    <div className="container">
-      <div className="row">
+          <div className="col-lg-3">
 
-        <div className="col-lg-3">
+            <SideMenu />
 
-          <SideMenu />
+          </div>
 
-        </div>
+          <div className="col-lg-9">
+            <Carousel />
 
-        <div className="col-lg-9">
-          <Carousel />
-
-          <div className="row">
-            <MovieList />
+            <div className="row">
+              <MovieList />
+            </div>
           </div>
         </div>
       </div>
     </div>
-    
-    <Footer />
 
+    <Footer />
+    <style jsx>{`
+      .home-page {
+        padding-top: 80px;
+      }
+    `}
+    </style>
   </div>
 );
 
