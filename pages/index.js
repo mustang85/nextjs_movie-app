92adbcb9047e29f1e4b9931cce0c306fa751c069
Granev Kirill <k.e.granev@gmail.com>
2019-11-17 21:50:17 +0300

    Get movies in class component

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, Component } from 'react'
 import Head from 'next/head';
 
 import Navbar from '../components/navbar';
@@ -9,66 +9,134 @@ import Footer from '../components/footer';
 
 import { getMovies } from '../actions';
 
-const Home = () => {
-  const [movies, setMovies] = useState([]);
-  const [count, setCount] = useState(0);
+// const Home = () => {
+//   const [movies, setMovies] = useState([]);
+//   const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    // getMovies().then((movies) => {
-    //   setMovies(movies);
-    // });
+//   useEffect(() => {
+//     // getMovies().then((movies) => {
+//     //   setMovies(movies);
+//     // });
 
-    const fetchData = async () => {
-      const resMovies = await getMovies();
-      setMovies(resMovies);
-    }
+//     const fetchData = async () => {
+//       const resMovies = await getMovies();
+//       setMovies(resMovies);
+//     }
 
-    fetchData();
-  }, [count])
+//     fetchData();
+//   }, [count])
 
-  return (
-    <div>
-      <Head>
-        <title>Home</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-      </Head>
+//   return (
+//     <div>
+//       <Head>
+//         <title>Home</title>
+//         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+//         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+//         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+//         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+//       </Head>
 
-      <Navbar />
+//       <Navbar />
       
-      <div className="home-page">
-        <div className="container">
-          <button onClick={() => setCount(count + 1)}>Click me!</button>
-          <div className="row">
+//       <div className="home-page">
+//         <div className="container">
+//           <button onClick={() => setCount(count + 1)}>Click me!</button>
+//           <div className="row">
 
-            <div className="col-lg-3">
+//             <div className="col-lg-3">
 
-              <SideMenu
-                appName={"Movie DB"} />
-            </div>
+//               <SideMenu
+//                 appName={"Movie DB"} />
+//             </div>
 
-            <div className="col-lg-9">
-              <Carousel />
+//             <div className="col-lg-9">
+//               <Carousel />
 
-              <div className="row">
-                <MovieList movies={movies} />
+//               <div className="row">
+//                 <MovieList movies={movies} />
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+
+//       <Footer />
+//       <style jsx>{`
+//         .home-page {
+//           padding-top: 80px;
+//         }
+//       `}
+//       </style>
+//     </div>
+//   )
+// };
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      movies: []
+    }
+  }
+
+  // Called only once when component is mounted!
+  // async componentDidMount() {
+  //   const movies = await getMovies();
+  //   this.setState({movies})
+  // }
+
+  componentDidMount() {
+    getMovies().then(movies => {
+      this.setState({movies})
+    })
+  }
+
+  render() {
+    const { movies } = this.state;
+    return (
+      <div>
+        <Head>
+          <title>Home</title>
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+          <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        </Head>
+  
+        <Navbar />
+        
+        <div className="home-page">
+          <div className="container">
+            <div className="row">
+  
+              <div className="col-lg-3">
+  
+                <SideMenu
+                  appName={"Movie DB"} />
+              </div>
+  
+              <div className="col-lg-9">
+                <Carousel />
+  
+                <div className="row">
+                  <MovieList movies={movies} />
+                </div>
               </div>
             </div>
           </div>
         </div>
+  
+        <Footer />
+        <style jsx>{`
+          .home-page {
+            padding-top: 80px;
+          }
+        `}
+        </style>
       </div>
-
-      <Footer />
-      <style jsx>{`
-        .home-page {
-          padding-top: 80px;
-        }
-      `}
-      </style>
-    </div>
-  )
+    )
+  }
 };
 
 export default Home;
