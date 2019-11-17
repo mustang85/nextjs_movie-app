2182fb8916bba6d34ed702d3bfa9e808aabd47cd
Granev Kirill <k.e.granev@gmail.com>
2019-11-17 21:34:42 +0300

    Use Effect

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Head from 'next/head';
 
 import Navbar from '../components/navbar';
@@ -11,10 +11,20 @@ import { getMovies } from '../actions';
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
+  const [count, setCount] = useState(0);
 
-  getMovies().then((movies) => {
-    setMovies(movies);
-  });
+  useEffect(() => {
+    // getMovies().then((movies) => {
+    //   setMovies(movies);
+    // });
+
+    const fetchData = async () => {
+      const resMovies = await getMovies();
+      setMovies(resMovies);
+    }
+
+    fetchData();
+  }, [count])
 
   return (
     <div>
@@ -30,6 +40,7 @@ const Home = () => {
       
       <div className="home-page">
         <div className="container">
+          <button onClick={() => setCount(count + 1)}>Click me!</button>
           <div className="row">
 
             <div className="col-lg-3">
