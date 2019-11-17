027725b79b83f4a351e02118928861aca6ec709a
Granev Kirill <k.e.granev@gmail.com>
2019-11-17 20:28:14 +0300

    Move movie data to Store

--- components/movieList.js
+++ components/movieList.js
@@ -12,9 +12,9 @@ class MovieList extends Component {
   renderMovies() {
     const { movies } = this.props;
 
-    return movies.map(({ name, description, image, rating }) => 
+    return movies.map(({ id, name, description, image, rating }) => 
       (
-        <div className="col-lg-4 col-md-6 mb-4">
+        <div key={id} className="col-lg-4 col-md-6 mb-4">
           <div className="card h-100">
             <a href="#"><img className="card-img-top" src={image} alt="" /></a>
             <div className="card-body">
