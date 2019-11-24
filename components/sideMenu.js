1b8d15a5ed6fcc286e585acc6c4d07a56f66c591
Granev Kirill <k.e.granev@gmail.com>
2019-11-24 22:45:55 +0300

    Modal implementation + Close

--- components/sideMenu.js
+++ components/sideMenu.js
@@ -1,8 +1,9 @@
-
+import Modal from './modal';
 
 const SideMenu = ({ appName, categories }) => {
   return (
     <div>
+      <Modal />
       <h1 className="my-4">{appName}</h1>
       <div className="list-group">
         { categories.map(c => (
